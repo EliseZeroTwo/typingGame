091423ae0d9d0e121bacffa396c7c833cbb37f7a
NaserAldeen <30509@aum.edu.kw>
2020-02-25 20:16:03 +0300

    hot login fix

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -7,7 +7,7 @@ export const login = userData => {
   return async dispatch => {
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/api/login/",
+        "https://api.touchtypo.com/api/login/",
         userData
       );
 
